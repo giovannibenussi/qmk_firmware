6d5a54bbf1df866af0e65bc11f627cdf6aa3db8d
Giovanni Benussi <giovanni.benussi@usach.cl>
2021-11-30 22:54:28 -0300

    Added Tailwind colors

--- keyboards/moonlander/keymaps/giovannibenussi/keymap.c
+++ keyboards/moonlander/keymaps/giovannibenussi/keymap.c
@@ -20,6 +20,7 @@
 
 #include QMK_KEYBOARD_H
 #include "version.h"
+#include "colors.h"
 
 enum layers {
     BASE,
@@ -41,6 +42,8 @@ enum custom_keycodes {
     TMUX_NEXT_WINDOW,
     TMUX_PREVIOUS_SESSION,
     TMUX_ZOOM,
+    TMUX_KILL,
+    TMUX_CREATE,
     TMUX_SCROLL,
     TMUX_ZOOM_SCROLL,
     ANIMATE_KEY_PRESS,
@@ -60,8 +63,8 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_moonlander(
     _______,         _______,               _______,      _______,      _______,      _______, _______,           _______,             _______,            _______,      _______,      _______,         _______,           _______,
-    KC_GRAVE,        KC_Q,                  KC_W,         KC_E,         KC_R,         KC_T,    _______,           _______,             KC_Y,               KC_U,         KC_I,         KC_O,            KC_P,              KC_SCOLON,
-    LCTL_T(KC_TAB),  LCMD_T(KC_A),          LCTL_T(KC_S), LOPT_T(KC_D), LSFT_T(KC_F), KC_G,    KC_NO,             KC_NO,               KC_H,               LSFT_T(KC_J), LOPT_T(KC_K), LCTL_T(KC_L),    LCMD_T(KC_BSPC),   KC_QUOT,
+    KC_GRAVE,        KC_Q,                  KC_W,         KC_E,         KC_R,         KC_T,    _______,           _______,             KC_Y,               KC_U,         KC_I,         KC_O,            KC_P,              KC_QUOT,
+    LCTL_T(KC_TAB),  LCMD_T(KC_A),          LCTL_T(KC_S), LOPT_T(KC_D), LSFT_T(KC_F), KC_G,    KC_NO,             KC_NO,               KC_H,               LSFT_T(KC_J), LOPT_T(KC_K), LCTL_T(KC_L),    LCMD_T(KC_BSPC),   KC_SCOLON,
     LSFT_T(KC_CAPS), LT(VIM_TMUX, KC_Z),    KC_X,         KC_C,         KC_V,         KC_B,                                            KC_N,               KC_M,         KC_COMM,      KC_DOT,          RCTL_T(KC_SLSH),   KC_RSFT,
     _______,         _______,               _______,     _______,      _______,      _______,                                         _______,             KC_LEAD,      TT(NUMPAD),      _______,         _______,           _______,
                                                                LT(SYMBOLS, KC_ENT),       LT(VIM_TMUX, KC_ESC),  KC_LGUI,           LT(RGB_LAYER, KC_ESC), KC_BSPC, LT(NUMBERS, KC_SPC)
@@ -96,11 +99,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [VIM_TMUX] = LAYOUT_moonlander(
-        _______, _______, _______,   _______,              _______,          _______,               _______,           _______, _______,  _______,   _______, _______, _______,  _______,
-        _______, _______, _______,   TMUX_PREVIOUS_WINDOW, TMUX_NEXT_WINDOW, TMUX_PREVIOUS_SESSION, _______,           _______, _______,  _______,   _______, _______, _______,  _______,
-        _______, _______, TMUX_ZOOM, VIM_TAB_PREV,         VIM_TAB_NEXT, TMUX_ZOOM_SCROLL,      _______,           _______, _______,  _______,   _______, _______, _______,  _______,
-        _______, _______, _______,   _______,              _______,          _______,                                  _______, _______,  _______,   _______, _______, _______,
-        _______, _______, _______,   TMUX_SCROLL,          _______,          _______,               _______,           _______, _______,  _______,   _______, _______,
+        _______, _______,   _______,     _______,              _______,          _______,               _______,           _______, _______,  _______,   _______, _______, _______,  _______,
+        _______, TMUX_KILL, _______,     TMUX_PREVIOUS_WINDOW, TMUX_NEXT_WINDOW, TMUX_PREVIOUS_SESSION, _______,           _______, _______,  _______,   _______, _______, _______,  _______,
+        _______, _______,   TMUX_ZOOM,   VIM_TAB_PREV,         VIM_TAB_NEXT, TMUX_ZOOM_SCROLL,      _______,           _______, _______,  _______,   _______, _______, _______,  _______,
+        _______, _______,   TMUX_CREATE, TMUX_SCROLL,              _______,          _______,                                  _______, _______,  _______,   _______, RESET, _______,
+        _______, _______,   _______,     _______,          _______,          _______,               _______,           _______, _______,  _______,   _______, _______,
                                             _______, _______, _______, _______,_______, _______
     ),
 
@@ -109,17 +112,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______,          _______, _______, KC_7,    KC_8,    KC_9,    _______, _______,
         _______, _______, _______, _______, _______, _______, _______,          _______, _______, KC_4,    KC_5,    KC_6,    _______, _______,
         _______, _______, _______, _______, _______, _______,                            _______, KC_1,    KC_2,    KC_3,    _______, _______,
-        _______, _______, _______, _______, _______,          _______, _______,          _______, KC_0,    _______, _______, _______,
+        _______, _______, _______, _______, _______,          _______, _______,          KC_0,    _______,    _______ , _______, _______,
                                             _______, _______, _______, _______, _______, KC_ENT
     ),
 
     [RGB_LAYER] = LAYOUT_moonlander(
-        _______, _______,        _______,          _______,          _______,        _______, _______,          _______,                    _______, _______, _______, _______, _______, _______,
-        _______, RGB_MODE_PLAIN, RGB_MODE_BREATHE, RGB_MODE_RAINBOW, RGB_MODE_SWIRL, _______, _______,          _______, ANIMATE_KEY_PRESS, _______, _______, _______, _______, _______,
-        _______, _______,        _______,          _______,          RGB_TOG,        _______, _______,          _______, RGB_HUD,           RGB_HUI, RGB_SAD, RGB_SAI, _______, _______,
-        _______, _______,        _______,          _______,          _______,        _______,                            _______,           _______, _______, _______, _______, _______,
-        _______, _______,        _______,          _______,          _______,                 _______, _______,          _______,           _______, _______, _______, _______,
-                                                                     _______,        _______, _______, _______, _______, _______
+        _______, _______,        _______,             _______,             _______,             _______, _______,          _______,                    _______, _______, _______, _______, _______, _______,
+        _______, RGB_MODE_PLAIN, RGB_MODE_BREATHE,    RGB_MODE_RAINBOW,    RGB_MODE_SWIRL,      RGB_TOG, _______,          _______, ANIMATE_KEY_PRESS, _______, _______, _______, _______, _______,
+        _______, _______,        KC_MEDIA_PREV_TRACK, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK, _______, _______,          _______, RGB_HUD,           RGB_HUI, RGB_SAD, RGB_SAI, _______, _______,
+        _______, _______,        _______,             KC__VOLDOWN,         KC__VOLUP,           _______,                            RGB_VAI,           RGB_VAD, _______, _______, _______, _______,
+        _______, _______,        _______,             _______,             _______,             _______, _______,          _______,           _______, _______, _______, _______,
+                                                                           _______,             _______, _______, _______, _______, _______
     ),
 
     /*[LAYOUT_NAME] = LAYOUT_moonlander(*/
@@ -168,6 +171,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case TMUX_ZOOM:
             SEND_STRING(SS_LCTL("s") "z");
             return false;
+        case TMUX_KILL:
+            SEND_STRING(SS_LCTL("s") "xy");
+            return false;
+        case TMUX_CREATE:
+            SEND_STRING(SS_LCTL("s") "c");
+            return false;
         case TMUX_SCROLL:
             SEND_STRING(SS_LCTL("s") SS_TAP(X_LBRACKET)); 
             return false;
@@ -203,10 +212,10 @@ enum combo_events {
 };
 uint16_t COMBO_LEN = COMBO_LENGTH; // remove the COMBO_COUNT define and use this instead!
 
-const uint16_t PROGMEM previous_tab[] = {LCTL_T(KC_S), LOPT_T(KC_D), COMBO_END};
-const uint16_t PROGMEM next_tab[] = {LCTL_T(KC_S), LSFT_T(KC_F), COMBO_END};
+const uint16_t PROGMEM previous_tab[] = {KC_X, KC_C, COMBO_END};
+const uint16_t PROGMEM next_tab[] = {KC_C, KC_V, COMBO_END};
 const uint16_t PROGMEM switch_app[] = {LCTL_T(KC_S), LOPT_T(KC_D), LSFT_T(KC_F), COMBO_END};
-const uint16_t PROGMEM vim_save[] = {LOPT_T(KC_D), LSFT_T(KC_F), COMBO_END};
+const uint16_t PROGMEM vim_save[] = {LCTL_T(KC_S), LSFT_T(KC_F), COMBO_END};
 const uint16_t PROGMEM vim_clipboard_register[] = {KC_U, KC_I, COMBO_END};
 
 combo_t key_combos[] = {
@@ -216,6 +225,80 @@ combo_t key_combos[] = {
   [VIM_SAVE] = COMBO_ACTION(vim_save),
   [VIM_CLIPBOARD_REGISTER] = COMBO_ACTION(vim_clipboard_register),
 };
+
+void set_home_row_color(uint8_t red, uint8_t green, uint8_t blue) {
+  rgb_matrix_set_color(7, red, green, blue);
+  rgb_matrix_set_color(12, red, green, blue);
+  rgb_matrix_set_color(17, red, green, blue);
+  rgb_matrix_set_color(22, red, green, blue);
+  rgb_matrix_set_color(43, red, green, blue);
+  rgb_matrix_set_color(48, red, green, blue);
+  rgb_matrix_set_color(53, red, green, blue);
+  rgb_matrix_set_color(58, red, green, blue);
+}
+
+void set_number_row_color(uint8_t red, uint8_t green, uint8_t blue) {
+  rgb_matrix_set_color(6, red, green, blue);
+  rgb_matrix_set_color(11, red, green, blue);
+  rgb_matrix_set_color(16, red, green, blue);
+  rgb_matrix_set_color(21, red, green, blue);
+  rgb_matrix_set_color(26, red, green, blue);
+  rgb_matrix_set_color(30, red, green, blue);
+  rgb_matrix_set_color(35, red, green, blue);
+  rgb_matrix_set_color(36, red, green, blue);
+  rgb_matrix_set_color(42, red, green, blue);
+  rgb_matrix_set_color(47, red, green, blue);
+  rgb_matrix_set_color(52, red, green, blue);
+  rgb_matrix_set_color(57, red, green, blue);
+  rgb_matrix_set_color(62, red, green, blue);
+}
+
+void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    switch(get_highest_layer(layer_state|default_layer_state)) {
+        case BASE:
+            rgb_matrix_set_color_all(RGB_TAILWIND_PURPLE_800);
+            set_home_row_color(RGB_TAILWIND_ROSE_500);
+            break;
+        case SYMBOLS:
+            rgb_matrix_set_color_all(RGB_TAILWIND_PURPLE_800);
+            set_number_row_color(RGB_TAILWIND_CYAN_500);
+            break;
+        case NUMBERS:
+            rgb_matrix_set_color_all(RGB_TAILWIND_PURPLE_900);
+            set_number_row_color(RGB_TAILWIND_ORANGE_500);
+            break;
+        case NUMPAD:
+            rgb_matrix_set_color_all(RGB_TAILWIND_PURPLE_900);
+            rgb_matrix_set_color(42, RGB_TAILWIND_GREEN_500);
+            rgb_matrix_set_color(43, RGB_TAILWIND_GREEN_500);
+            rgb_matrix_set_color(44, RGB_TAILWIND_GREEN_500);
+            rgb_matrix_set_color(47, RGB_TAILWIND_GREEN_500);
+            rgb_matrix_set_color(48, RGB_TAILWIND_GREEN_500);
+            rgb_matrix_set_color(49, RGB_TAILWIND_GREEN_500);
+            rgb_matrix_set_color(52, RGB_TAILWIND_GREEN_500);
+            rgb_matrix_set_color(53, RGB_TAILWIND_GREEN_500);
+            rgb_matrix_set_color(54, RGB_TAILWIND_GREEN_500);
+            rgb_matrix_set_color(57, RGB_TAILWIND_GREEN_500);
+            rgb_matrix_set_color(58, RGB_TAILWIND_GREEN_500);
+            rgb_matrix_set_color(59, RGB_TAILWIND_GREEN_500);
+            rgb_matrix_set_color(60, RGB_TAILWIND_LIME_500);
+            break;
+        case VIM_TMUX:
+            rgb_matrix_set_color_all(RGB_TAILWIND_ROSE_600);
+            rgb_matrix_set_color(12, RGB_TAILWIND_TEAL_500);
+            rgb_matrix_set_color(16, RGB_TAILWIND_PURPLE_800);
+            rgb_matrix_set_color(17, RGB_TAILWIND_TEAL_500);
+            rgb_matrix_set_color(18, RGB_TAILWIND_GREEN_500);
+            rgb_matrix_set_color(21, RGB_TAILWIND_PURPLE_800);
+            rgb_matrix_set_color(22, RGB_TAILWIND_TEAL_500);
+            rgb_matrix_set_color(26, RGB_TAILWIND_PURPLE_800);
+            rgb_matrix_set_color(27, RGB_TAILWIND_TEAL_500);
+            rgb_matrix_set_color(44, RGB_TAILWIND_TEAL_500);
+            break;
+        default:
+            break;
+    }
+}
 
 void process_combo_event(uint16_t combo_index, bool pressed) {
   switch(combo_index) {
@@ -231,6 +314,17 @@ void process_combo_event(uint16_t combo_index, bool pressed) {
       break;
     case SWITCH_APP_COMBO:
       if (pressed) {
+        rgb_matrix_set_color(0, RGB_RED);
+        rgb_matrix_set_color(1, RGB_RED);
+        rgb_matrix_set_color(2, RGB_RED);
+        rgb_matrix_set_color(3, RGB_RED);
+        rgb_matrix_set_color(4, RGB_RED);
+        rgb_matrix_set_color(5, RGB_RED);
+        rgb_matrix_set_color(6, RGB_RED);
+        rgb_matrix_set_color(7, RGB_RED);
+        rgb_matrix_set_color(8, RGB_RED);
+        rgb_matrix_set_color(9, RGB_RED);
+        rgb_matrix_set_color(10, RGB_RED);
         SEND_STRING(SS_DOWN(X_LCMD) SS_TAP(X_TAB) SS_UP(X_LCMD)); 
       }
       break;
@@ -254,12 +348,8 @@ void matrix_scan_user(void) {
     leading = false;
     leader_end();
 
-    SEQ_ONE_KEY(KC_F) {
-      // Anything you can do in a macro.
-      SEND_STRING("QMK is awesome.");
-    };
-    SEQ_TWO_KEYS(KC_D, KC_D) {
-      SEND_STRING(SS_LCTL("a") SS_LCTL("c"));
+    SEQ_TWO_KEYS(KC_E, KC_M) {
+      SEND_STRING("giovanni.benussi@usach.cl");
     };
     SEQ_THREE_KEYS(KC_O, KC_P, KC_R) {
         SEND_STRING(SS_DOWN(X_LOPT) SS_DOWN(X_LSFT) "v" SS_UP(X_LSFT) SS_UP(X_LOPT)); 
