2d20cd7ee916073a9614dd6f030d1b38303bcad5
Giovanni Benussi <giovanni.benussi@usach.cl>
2023-11-13 08:16:49 -0300

    Add shortcut for r

--- keyboards/moonlander/keymaps/giovannibenussi/keymap.c
+++ keyboards/moonlander/keymaps/giovannibenussi/keymap.c
@@ -70,6 +70,7 @@ enum custom_keycodes {
     OPEN_APPLICATION_Q,
     OPEN_APPLICATION_W,
     OPEN_APPLICATION_E,
+    OPEN_APPLICATION_R,
     OPEN_APPLICATION_A,
     OPEN_APPLICATION_S,
     OPEN_APPLICATION_D,
@@ -182,7 +183,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [APPS_LAYER] = LAYOUT_moonlander(
         _______,                   _______,             _______,            _______,            _______,            _______,            _______,          _______, _______, _______, _______, _______, _______, _______,
-        _______,                   OPEN_APPLICATION_Q,  OPEN_APPLICATION_W, OPEN_APPLICATION_E, _______,            _______,            _______,          _______, _______, _______, OPEN_APPLICATION_I, _______, _______, _______,
+        _______,                   OPEN_APPLICATION_Q,  OPEN_APPLICATION_W, OPEN_APPLICATION_E, OPEN_APPLICATION_R, _______,            _______,          _______, _______, _______, OPEN_APPLICATION_I, _______, _______, _______,
         OPEN_PREVIOUS_APPLICATION, OPEN_APPLICATION_A,  OPEN_APPLICATION_S, OPEN_APPLICATION_D, OPEN_APPLICATION_F, OPEN_APPLICATION_G,            _______,          _______, _______, _______, _______, OPEN_APPLICATION_L, _______, _______,
         KC_MEDIA_PLAY_PAUSE,       OPEN_APPLICATION_Z,  OPEN_APPLICATION_X, OPEN_APPLICATION_C, OPEN_APPLICATION_V, OPEN_APPLICATION_B,                            OPEN_APPLICATION_N, _______, _______, _______, _______, _______,
         KC_MEDIA_PREV_TRACK,       KC_MEDIA_NEXT_TRACK, KC__VOLDOWN,        KC__VOLUP,                              _______  ,          _______, _______,          _______, _______, _______, _______, _______,
@@ -318,6 +319,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return false;
         case OPEN_APPLICATION_E:
             open_application("e");
+            return false;
+        case OPEN_APPLICATION_R:
+            open_application("r");
             return false;
         case OPEN_APPLICATION_A:
             open_application("a");
